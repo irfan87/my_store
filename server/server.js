a01a8ac74a6373e415f05a350bec0bb59bffadb1
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-26 21:36:40 +0800

    setup nodemon, concurrently and .env

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 const products = require("./products");
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +20,11 @@ app.get("/api/products/:id", (req, res) => {
 	res.status(200).json(product);
 });
 
-app.listen(5000, console.log("Server is running on port 5000"));
+// verify the port
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV;
+
+app.listen(
+	PORT,
+	console.log(`Server is running in ${MODE} mode on port ${PORT}`)
+);
