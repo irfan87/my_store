53a000ca93ea9762d16abf722dfe20b11f22e0c0
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-26 22:50:16 +0800

    setup mongodb and applied colors package to show the message at the terminal / command prompt

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+
+import connectDB from "./config/db.js";
 
 import products from "./products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -26,5 +31,7 @@ const MODE = process.env.NODE_ENV;
 
 app.listen(
 	PORT,
-	console.log(`Server is running in ${MODE} mode on port ${PORT}`)
+	console.log(
+		`Server is running in ${MODE} mode on port ${PORT}`.magenta.underline.bold
+	)
 );
