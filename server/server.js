82d99e2e9706197a785e7788dd7c38249716ce28
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-26 11:37:15 +0800

    setup backend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const products = require("./products");
+
+const app = express();
+
+// route for test
+app.get("/", (req, res) => res.send("hello world!"));
+
+// get the product
+app.get("/api/products", (req, res) => {
+	res.status(200).json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.status(200).json(product);
+});
+
+app.listen(5000, console.log("Server is running on port 5000"));
