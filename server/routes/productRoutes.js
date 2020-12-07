6ba393ee855de4eb325bcce6b8c58577759e999a
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-12-07 21:41:24 +0800

    refactored routes to the controller

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,37 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-
-import Product from "../models/productModel.js";
+import {
+	getProductById,
+	getProducts,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// @desc FETCH ALL PRODUCTS
-// @route GET /api/products
-// @access Public Route
-router.get(
-	"/",
-	asyncHandler(async (req, res) => {
-		const products = await Product.find({});
-		res.status(200).json(products);
-	})
-);
-
-// @desc FETCH SINGLE PRODUCTS
-// @route GET /api/products/1
-// @access Public Route
-router.get(
-	"/:id",
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-
-		if (product) {
-			res.status(200).json(product);
-		} else {
-			res.status(404);
-
-			throw new Error("Product not found");
-		}
-	})
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
